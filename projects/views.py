a7280c99233c58803771f8388b31502ad9ba4145
Jacek <J.Dobrowolski@stud.elka.pw.edu.pl>
2023-02-08 20:57:26 +0100

    the basics

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+projectsList = [
+    {'id': '1', 'title': 'E-commerce Website', 'description': 'Fully functional e-commerce website' },
+    { 'id': '2', 'title': 'Portfolio Website', 'description': 'A personal website to write articles and display work' },
+    {'id': '3', 'title': 'Social Network', 'description': 'An open source project built by the community' }
+]
+
+
+def projects(request):
+    page = 'projects'
+    number = 10
+    context = {
+        'page': page,
+        'number': number,
+        'projects': projectsList,
+    }
+    return render(request=request,
+                  template_name='projects/projects.html',
+                  context=context)
+
+def project(request, pk):
+    projects = {project['id']: project for project in projectsList if project['id'] == pk}
+    if pk not in projects:
+        return HttpResponseNotFound()
+
+    return render(request=request,
+                template_name='projects/single-project.html',
+                context={'project': projects[pk]})
