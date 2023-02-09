08027e441c916c2f5bc55824437d5be79e5bffdd
Jacek <J.Dobrowolski@stud.elka.pw.edu.pl>
2023-02-09 19:23:21 +0100

    crud

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from projects.models import Project
 from projects.forms import ProjectForm
+
 
 def projects(request):
     projects_query = Project.objects.all()
@@ -11,6 +12,7 @@ def projects(request):
     return render(request=request,
                   template_name='projects/projects.html',
                   context=context)
+
 
 def project(request, pk):
     try:
@@ -25,5 +27,33 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(projects)
+
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(project_id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(projects)
+
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(project_id=pk)
+    context = {'object': project}
+    if request.method == 'POST':
+        project.delete()
+        return redirect(projects)
+    return render(request, 'delete_template.html', context)
